08770cf022cce904a345e4a8c90841a9937f795d
Colin Sullivan <colin.sullivan@apcera.com>
2016-10-31 15:54:21 -0600

    emulate real clients

--- stress/stan/stan-scale.go
+++ stress/stan/stan-scale.go
@@ -11,6 +11,8 @@ import (
 	"sync"
 	"time"
 
+	"math/rand"
+
 	"github.com/nats-io/go-nats-streaming"
 	"github.com/nats-io/nats"
 	"github.com/nats-io/nats/bench"
@@ -26,12 +28,91 @@ const (
 	DefaultMessageSize        = 128
 	DefaultIgnoreOld          = false
 	DefaultMaxPubAcksInflight = 1000
-	DefaultClientID           = "benchmark"
-	DefaultConnectWait        = 20 * time.Second
+	DefaultClientID           = "scale"
+	DefaultConnectWait        = 120 * time.Second
 )
 
 func usage() {
-	log.Fatalf("Usage: stan-scale-test [-s server (%s)] [--tls] [-id CLIENT_ID] [-np NUM_PUBLISHERS] [-ns NUM_SUBSCRIBERS] [-n NUM_MSGS] [-ms MESSAGE_SIZE] [-nc NUMCONNS] [-csv csvfile] [-mpa MAX_NUMBER_OF_PUBLISHED_ACKS_INFLIGHT] [-io] [-a] <subject>\n", nats.DefaultURL)
+	log.Fatalf("Usage: stan-scale [-s server (%s)] [--tls] [-id CLIENT_ID] [-np NUM_PUBLISHERS] [-ns NUM_SUBSCRIBERS] [-n NUM_MSGS] [-ms MESSAGE_SIZE] [-nc NUMCONNS] [-csv csvfile] [-mpa MAX_NUMBER_OF_PUBLISHED_ACKS_INFLIGHT] [-io] [-a] <subject>\n", nats.DefaultURL)
+}
+
+func disconnectedHandler(nc *nats.Conn) {
+	if nc.LastError() != nil {
+		log.Fatalf("connection %q has been disconnected: %v",
+			nc.Opts.Name, nc.LastError())
+	}
+}
+
+func reconnectedHandler(nc *nats.Conn) {
+	log.Fatalf("connection %q reconnected to NATS Server at %q",
+		nc.Opts.Name, nc.ConnectedUrl())
+}
+
+func closedHandler(nc *nats.Conn) {
+	log.Fatalf("connection %q has been closed", nc.Opts.Name)
+}
+
+func errorHandler(nc *nats.Conn, sub *nats.Subscription, err error) {
+	log.Fatalf("asynchronous error on connection %s, subject %s: %s",
+		nc.Opts.Name, sub.Subject, err)
+}
+
+type subTrack struct {
+	sync.Mutex
+	subsMap map[string]int
+}
+
+func newSubTrack() *subTrack {
+	newst := &subTrack{}
+	newst.subsMap = make(map[string]int)
+	return newst
+}
+
+// global subscription tracker
+var st *subTrack
+
+func (s *subTrack) initSubscriber(subject string) {
+	s.Lock()
+	defer s.Unlock()
+	// for future recycling
+	if _, ok := s.subsMap[subject]; ok {
+		s.subsMap[subject]++
+	} else {
+		s.subsMap[subject] = 1
+	}
+}
+
+func (s *subTrack) completeSubscriber(subject string) {
+	s.Lock()
+	defer s.Unlock()
+
+	if _, ok := s.subsMap[subject]; ok {
+		s.subsMap[subject]--
+		if s.subsMap[subject] == 0 {
+			delete(s.subsMap, subject)
+		}
+	}
+}
+
+func (s *subTrack) printUnfinishedCount() bool {
+	s.Lock()
+	defer s.Unlock()
+
+	log.Printf("Remaining Subscribers (%d)\n", len(s.subsMap))
+
+	return len(s.subsMap) == 0
+}
+
+func (s *subTrack) printUnfinishedDetail(max int) {
+
+	i := 0
+	for subj, remaining := range s.subsMap {
+		log.Printf("    %s;%d", subj, remaining)
+		i++
+		if i == max {
+			break
+		}
+	}
 }
 
 var conns []*nats.Conn
@@ -47,6 +128,7 @@ func buildConns(count int, opts *nats.Options) error {
 	}
 	conns = make([]*nats.Conn, count)
 	for i := 0; i < count; i++ {
+		opts.Name = "conn-" + string(i)
 		conns[i], err = opts.Connect()
 	}
 	return err
@@ -76,6 +158,11 @@ func getNextNatsConn() *nats.Conn {
 var currentSubjCount int
 var useUniqueSubjects bool
 
+func resetSubjects() {
+	currentSubjCount = 0
+}
+
+// TODO:  is recycling subjects necessary?
 func getNextSubject(baseSubject string, max int) string {
 	if !useUniqueSubjects {
 		return baseSubject
@@ -120,13 +207,20 @@ func main() {
 	useUniqueSubjects = *uniqueSubjs
 	verbose = *vb
 
+	st = newSubTrack()
+
 	// Setup the option block
 	opts := nats.DefaultOptions
 	opts.Servers = strings.Split(*urls, ",")
 	for i, s := range opts.Servers {
 		opts.Servers[i] = strings.Trim(s, " ")
 	}
+
 	opts.Secure = *tls
+	opts.AsyncErrorCB = errorHandler
+	opts.DisconnectedCB = disconnectedHandler
+	opts.ReconnectedCB = reconnectedHandler
+	opts.ClosedCB = closedHandler
 
 	if err := buildConns(*numConns, &opts); err != nil {
 		log.Fatalf("Unable to create connections: %v", err)
@@ -135,31 +229,43 @@ func main() {
 	benchmark = bench.NewBenchmark("NATS Streaming", *numSubs, *numPubs)
 
 	var startwg sync.WaitGroup
-	var donewg sync.WaitGroup
+	var pubwg sync.WaitGroup
+	var subwg sync.WaitGroup
 
-	donewg.Add(*numPubs + *numSubs)
+	subwg.Add(*numSubs)
+	pubwg.Add(*numPubs)
 
 	// Run Subscribers first
 	startwg.Add(*numSubs)
 	for i := 0; i < *numSubs; i++ {
 		subID := fmt.Sprintf("%s-sub-%d", *clientID, i)
-		go runSubscriber(&startwg, &donewg, opts, *numMsgs, *messageSize, *ignoreOld, subID, getNextSubject(args[0], *numSubs))
+		go runSubscriber(&startwg, &subwg, opts, *numMsgs, *messageSize, *ignoreOld, subID, getNextSubject(args[0], *numSubs))
 	}
 	startwg.Wait()
 
+	log.Printf("Starting scaling test [msgs=%d, msgsize=%d, pubs=%d, subs=%d]\n", *numMsgs, *messageSize, *numPubs, *numSubs)
 	// Now Publishers
 	startwg.Add(*numPubs)
 	pubCounts := bench.MsgsPerClient(*numMsgs, *numPubs)
 	for i := 0; i < *numPubs; i++ {
 		pubID := fmt.Sprintf("%s-pub-%d", *clientID, i)
-		go runPublisher(&startwg, &donewg, opts, pubCounts[i], *messageSize, *async, pubID, *maxPubAcks, args[0], *numSubs)
+		go runPublisher(&startwg, &pubwg, opts, pubCounts[i], *messageSize, *async, pubID, *maxPubAcks, args[0], *numSubs)
 	}
 
-	log.Printf("Starting benchmark [msgs=%d, msgsize=%d, pubs=%d, subs=%d]\n", *numMsgs, *messageSize, *numPubs, *numSubs)
-
 	startwg.Wait()
-	donewg.Wait()
+	pubwg.Wait()
 
+	log.Println("Done publishing.")
+
+	isFinished := st.printUnfinishedCount()
+	// 10 mins is a long time to wait, but for stress tests see if they recover.
+	for i := 0; i < 600 && !isFinished; i++ {
+		st.printUnfinishedDetail(5)
+		time.Sleep(1 * time.Second)
+		isFinished = st.printUnfinishedCount()
+	}
+
+	subwg.Wait()
 	benchmark.Close()
 	fmt.Print(benchmark.Report())
 
@@ -176,6 +282,9 @@ func publishMsgs(snc stan.Conn, msg []byte, async bool, numMsgs int, subj string
 	if async {
 		ch := make(chan bool)
 		acb := func(lguid string, err error) {
+			if err != nil {
+				log.Fatalf("Publish error: %v\n", err)
+			}
 			published++
 			if published >= numMsgs {
 				ch <- true
@@ -196,6 +305,46 @@ func publishMsgs(snc stan.Conn, msg []byte, async bool, numMsgs int, subj string
 			}
 			published++
 		}
+	}
+}
+
+// publishUniqueMsgs distributes messages evenly across subjects
+func publishMsgsOnUniqueSubjects(snc stan.Conn, msg []byte, async bool, numMsgs int, subj string, numSubs int) {
+	var published int
+
+	ch := make(chan bool)
+	acb := func(lguid string, err error) {
+		if err != nil {
+			log.Fatalf("Publish error: %v\n", err)
+		}
+		published++
+		if published >= numMsgs {
+			ch <- true
+		}
+	}
+
+	for i := 0; i < numMsgs; i++ {
+		resetSubjects()
+		for j := 0; j < numSubs; j++ {
+			sub := getNextSubject(subj, numSubs)
+			if async {
+				_, err := snc.PublishAsync(sub, msg, acb)
+				if err != nil {
+					log.Fatalf("Publish error: %v\n", err)
+				}
+			} else {
+				err := snc.Publish(sub, msg)
+				if err != nil {
+					log.Fatalf("Publish error: %v\n", err)
+				}
+				published++
+			}
+		}
+	}
+
+	// wait for publishers
+	if async {
+		<-ch
 	}
 }
 
@@ -225,9 +374,7 @@ func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs in
 	start := time.Now()
 
 	if useUniqueSubjects {
-		for i := 0; i < numSubs; i++ {
-			publishMsgs(snc, msg, async, numMsgs, fmt.Sprintf("%s.%d", subj, i))
-		}
+		publishMsgsOnUniqueSubjects(snc, msg, async, numMsgs, subj, numSubs)
 	} else {
 		publishMsgs(snc, msg, async, numMsgs, subj)
 	}
@@ -235,10 +382,6 @@ func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs in
 	benchmark.AddPubSample(bench.NewSample(numMsgs, msgSize, start, time.Now(), snc.NatsConn()))
 	snc.Close()
 	donewg.Done()
-
-	if verbose {
-		fmt.Println("Done publishing.")
-	}
 }
 
 func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs int, msgSize int, ignoreOld bool, subID, subj string) {
@@ -255,24 +398,28 @@ func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs i
 		log.Fatalf("Subscriber %s can't connect: %v\n", subID, err)
 	}
 
+	time.Sleep(time.Duration(rand.Intn(10000)) * time.Millisecond)
+
 	ch := make(chan bool)
 	start := time.Now()
 
+	st.initSubscriber(subj)
 	received := 0
 	mcb := func(msg *stan.Msg) {
 		received++
 		if received >= numMsgs {
-			if verbose {
-				fmt.Printf("Done receiving on %s.\n", msg.Subject)
-			}
+			/*f verbose {
+				log.Printf("Done receiving on %s.\n", msg.Subject)
+			}*/
+			st.completeSubscriber(subj)
 			ch <- true
 		}
 	}
 
 	if ignoreOld {
-		snc.Subscribe(subj, mcb)
+		snc.Subscribe(subj, mcb, stan.AckWait(time.Second*120))
 	} else {
-		snc.Subscribe(subj, mcb, stan.DeliverAllAvailable())
+		snc.Subscribe(subj, mcb, stan.DeliverAllAvailable(), stan.AckWait(time.Second*120))
 	}
 	startwg.Done()
 
