57a07d022203214b6833c26f79b51bc20adc5d3c
Colin Sullivan <colin.sullivan@apcera.com>
2016-11-21 11:43:07 -0700

    Updates on dependencies

--- monitor/client/monitor.sh
+++ monitor/client/monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 kill `ps -a | grep collectd_export | cut -c 1-6`
-nohup go/bin/collectd_exporter -collectd.listen-address=":25826" &
+nohup $HOME/gopath/bin/collectd_exporter -collectd.listen-address=":25826" &
--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,18 +3,20 @@
 #
 # Convenience script should be copied to the test machine
 #
-
 sudo apt update
-sudo apt install git
 sudo apt install collectd
 sudo cp collectd.conf /etc/collectd/collectd.conf
 sudo service collectd restart
 
 wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
-sudo tar -zxvf go1.7.1.linux-amd64.tar.gz -C /usr/local/
+tar -zxvf go1.7.1.linux-amd64.tar.gz -C $HOME
 
-mkdir go 2>/dev/null
-export GOPATH=~/go
+export PATH=$HOME/go/bin:$PATH
+export GOPATH=$HOME/gopath
+
+echo "Getting gnatsd"
 go get github.com/nats-io/gnatsd
+
+echo "Getting promethous exporter"
 go get github.com/prometheus/collectd_exporter
 
