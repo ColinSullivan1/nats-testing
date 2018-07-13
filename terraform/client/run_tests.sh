a57c1694305521ee2ac9b304108e710d697359d9
Colin Sullivan <colin@synadia.com>
2018-07-13 16:50:56 -0600

    Fix script, minor updates, add notes

--- terraform/client/run_tests.sh
+++ terraform/client/run_tests.sh
@@ -2,15 +2,15 @@
 
 . ./setenv.sh
 
-dur = "10s"
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 256 -tr 1000 -tt $dur -hist lat_256b_1kmps
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 256 -tr 10000 -tt $dur -hist lat_256b_10kmps
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 256 -tr 100000 -tt $dur -hist lat_256b_100kmps
+dur="10s"
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 256 -tr 1000 -tt $dur -hist lat_256b_1kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 256 -tr 10000 -tt $dur -hist lat_256b_10kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 256 -tr 100000 -tt $dur -hist lat_256b_100kmps
 
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 1024 -tr 1000 -tt $dur -hist lat_1k_1kmps
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 1024 -tr 10000 -tt $dur -hist lat_1k_10kmps
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 1024 -tr 100000 -tt $dur -hist lat_1k_100kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 1024 -tr 1000 -tt $dur -hist lat_1k_1kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 1024 -tr 10000 -tt $dur -hist lat_1k_10kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 1024 -tr 100000 -tt $dur -hist lat_1k_100kmps
 
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 4096 -tr 1000 -tt $dur -hist lat_4k_1kmps
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 4096 -tr 10000 -tt $dur -hist lat_4k_10kmps
-latency-tests -sa nats://server.nats.a:4222 -sb nats://server.nats.b:4222 -sz 4096 -tr 100000 -tt $dur -hist lat_4k_100kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 4096 -tr 1000 -tt $dur -hist lat_4k_1kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 4096 -tr 10000 -tt $dur -hist lat_4k_10kmps
+latency-tests -sa nats://nats.server.a:4222 -sb nats://nats.server.b:4222 -sz 4096 -tr 100000 -tt $dur -hist lat_4k_100kmps
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,23 @@
 # User must define this
 variable auth_token {}
 
+#
+# Machine type reference (July 2018)
+#
+# "c2.medium.x86","c2.medium.x86", "Our c2.medium.x86 configuration is an AMD 7401P EPYC server.",
+# "m2.xlarge.x86","m2.xlarge.x86","Our m2.xlarge.x86 configuration is a...",
+# "storage_1","Standard","TBD"
+# "storage_2","Performance","TBD",
+# "baremetal_2a","c1.large.arm","Our Type 2A configuration is a 96-core dual socket ARM 64 beast based on Cavium ThunderX chips",
+# "baremetal_1","c1.small.x86","Our Type 1 configuration is a zippy general use server, with an Intel E3-1240 v3 processor and 32GB of RAM.",
+# "baremetal_3","c1.xlarge.x86","Our Type 3 configuration is a high core, high IO server, with dual Intel E5-2640 v3 processors, 128GB of DDR4 RAM and ultra fast NVME flash drives.",
+# "baremetal_2","m1.xlarge.x86","Our Type 2 configuration is the perfect all purpose virtualization server, with dual E5-2650 v4 processors, 256 GB of DDR4 RAM, and six SSDs totaling 2.8 TB of storage.",
+# "baremetal_s","s1.large.x86","Our Type S server packs in 24TB of storage and is perfect for large object or file needs.",
+# "baremetal_0","t1.small.x86", "Our Type 0 configuration is a general use \"cloud killer\" server, with a Intel Atom 2.4Ghz processor and 8GB of RAM.",
+# "baremetal_1e", "x1.small.x86","Our Type 1e ...",
+
 variable latency_server_type {
-  default = "baremetal_1"
+  default = "c2.medium.x86"
 }
 
 variable latency_client_type {
